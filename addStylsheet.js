b7e91703347c5202dc3801064d30dac42b8f04aa
Jonathan Fogelström <jonathanfogelstrom@hotmail.se>
2018-01-17 13:33:21 +0100

    initital commit

---
+++ addStylsheet.js
@@ -0,0 +1,6 @@
+﻿function addStylesheet(url) {
+    var newStylesheet = document.createElement("link");
+    newStylesheet.setAttribute("rel", "stylesheet");
+    newStylesheet.setAttribute("href", url);
+    document.head.insertAdjacentElement("afterbegin", newStylesheet);
+}
---
+++ common.js
@@ -0,0 +1,45 @@
+﻿var header = document.getElementsByTagName("header")[0];
+var headerSection = document.getElementsByClassName("headerSection");
+var promotionBtn = document.getElementsByClassName("promotionBtn");
+var promoPlaceholder = document.getElementById("promoPlaceholder");
+
+/*var currentScrollPos = document.documentElement.scrollTop;
+var isTrue = true;*/
+
+document.body.onscroll = function () {
+    if (document.documentElement.clientWidth > 1000) {
+        if (document.documentElement.scrollTop > 0) {
+            for (var i = 0; i < headerSection.length; i++) {
+                headerSection[i].classList.add("scroll");
+                promotionBtn[i].classList.add("scroll");
+            }
+            headerSection[1].classList.add("float-right");
+            document.getElementsByTagName("header")[0].classList.add("scroll");
+        } else {
+            for (var i = 0; i < headerSection.length; i++) {
+                headerSection[i].classList.remove("scroll");
+                promotionBtn[i].classList.remove("scroll");
+            }
+            headerSection[1].classList.remove("float-right");
+            document.getElementsByTagName("header")[0].classList.remove("scroll");
+        }
+    } else {
+        if (document.documentElement.scrollTop > headerSection[0].offsetHeight) {
+            header.style.position = "fixed";
+            header.style.top = "-" + headerSection[0].offsetHeight + "px";
+        } else {
+            header.style.position = "absolute";
+            header.style.top = "0";
+        }
+    }
+    /*if (document.documentElement.scrollTop < currentScrollPos) {
+        if (isTrue == true) {
+            header.style.position = "absolute";
+            header.style.top = document.documentElement.scrollTop - headerSection[0].offsetHeight + "px";
+            isTrue = false;
+        }
+    } else {
+        istrue = true;
+    }
+    currentScrollPos = document.documentElement.scrollTop;*/
+}
